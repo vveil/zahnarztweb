6de127a1b7c79d125246b67738efc6121de75d14
Niklas <account@niklas.ai>
2023-12-22 17:29:00 +0100

    added impressum-route

---
+++ src/routes/impressum.tsx
@@ -0,0 +1,63 @@
+import { A } from "solid-start";
+import LandingPage from "~/components/LandingPage";
+import MobileLandingPage from "~/components/MobileLandingPage";
+import BaseLayout from "~/layouts/BaseLayout";
+
+export default function Home() {
+  return (
+    <main class="">
+      <BaseLayout className="">
+        <div class="flex flex-col gap-4 mx-8">
+
+          <h1 class="text-4xl">Impressum</h1>
+
+          <div>
+            Angaben gemäß § 5 TMG <br />
+            Zahnarztpraxis Dr.med.dent. Jürgen Werner <br />
+            Weststraße 70 <br />
+            38126 Braunschweig <br />
+            Berufsbezeichnung Zahnarzt <br />
+            (Verliehen in der Bundesrepublik Deutschland)
+          </div>
+          <div>
+            <span class="italic">Kontakt:</span> <br />
+            Telefon 0531-6802929 <br />
+            Telefax 0531-6802930 <br />
+            E-Mail dr.juergenwerner@arcor.de <br />
+            Internet dr-werner-zahnarzt.de <br />
+          </div>
+          <div>
+            <span class="italic">Aufsichtsbehörde:</span> <br />
+            Zahnärztekammer Niedersachsen <br />
+            Zeißstraße 11a <br />
+            30519 Hannover  <br />
+            Zuständige kassenzahnärztliche Vereinigung: <br />
+            Kassenzahnärztliche Vereinigung Niedersachsen <br />
+            Zeißstraße 11 <br />
+            30519 Hannover <br />
+          </div>
+
+          <div>
+            <span class="italic">Haftungsausschluss:</span> <br />
+            Gemäß § 7 Abs. 1 TMG sind wir für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 - 10 TMG sind wir jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidirge Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechnenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+            Haftung für Links
+            Unsere Seite enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese frendem Inhalte auch keine Gewähr übernhemen. Für Inhalte der verlinkten Seiten ist stets der jeweilige Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft, rechtswidrige Inhalte waren nicht erkennbar.
+            Urheberrecht:
+            Die Inhalte auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Verfielfältigung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung. Downloads und Kopien dieser Seite sind nur für den privaten Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, sin die Urheberrechte Dritter zu beachten.
+          </div>
+
+          <div>
+            <h1 class="text-2xl">Datenschutzerklärung</h1>
+            Datenschutzerklärung nach DSGVO
+            Die Nutzung dieser Webseite ist ohne Angabe von personenbezogenen Daten möglich.
+            Datenschutzerklärung für die Nutzung von Youtube:
+            Unsere Webseite nutzt Plugins der von Google betriebenen Seite Youtube. Betreiber der Seiten ist die YouTube, LLC, 901 Cherry Ave., San Bruno, CA 94066,USA. Wenn Sie eine unserer mit einem YouTube-Plugin ausgesatteten Seiten besuchen, wird eine Verbindung zu den Servern von YouTube hergestellt. Weitere Infromationen zum Umgang mit Nutzerdaten finden Sie in der Datenschutzerklärung von YouTube unter https://www.google.de/intl/de/policies/privacy.
+            Kontaktformular:
+            Die bei Nutzung des Kontaktformulares angegebenen Daten werden zwecks Bearbeitung der Anfrage bei uns gespeichert. Eine Weitergabe der Daten erfolgt nicht
+            (siehe Datenschutzverordnung nach DSGVO).
+          </div>
+        </div>
+      </BaseLayout >
+    </main >
+  );
+}
