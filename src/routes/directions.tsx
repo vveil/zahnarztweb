7822345ede140cce841f99b4c4a4696a5f2ef753
Niklas <account@niklas.ai>
2023-12-23 11:50:39 +0100

    added directions as test

---
+++ src/routes/directions.tsx
@@ -0,0 +1,14 @@
+import { A } from "solid-start";
+import LandingPage from "~/components/LandingPage";
+import MobileLandingPage from "~/components/MobileLandingPage";
+import BaseLayout from "~/layouts/BaseLayout";
+
+export default function Directions() {
+  return (
+    <main class="">
+      <BaseLayout className="">
+        <p>Rautheim</p>
+      </BaseLayout >
+    </main >
+  );
+}
