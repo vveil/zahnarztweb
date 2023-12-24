f6c90d88aadd3b36d9ea20758a85d610012f4e46
Niklas <account@niklas.ai>
2023-12-24 11:54:44 +0100

    added services.json and added services to home page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,26 +2,30 @@ import ApplicationTeaser from "./ApplicationTeaser";
 import Contact from "./Contact";
 import HeroText from "./HeroText";
 import OpeningHours from "./OpeningHours";
+import ServicesShort from "./ServicesShort";
 import TeamImage from "./TeamImage";
 
 export default function LandingPage(props: any) {
     return (
-        <div class={`${props.className} hidden sm:flex flex-col lg:flex-row justify-between pb-4 h-auto w-auto`}>
-            <div class="flex flex-col gap-4 md:gap-8 ">
-                <HeroText />
-                <div class="text-xl">
-                    <p>Weststr. 70</p>
-                    <p>38126 Braunschweig</p>
+        <div class={`${props.className} flex flex-col gap-4`}>
+            <div class={`hidden sm:flex flex-col lg:flex-row justify-between pb-4 h-auto w-auto`}>
+                <div class="flex flex-col gap-4 md:gap-8 ">
+                    <HeroText />
+                    <div class="text-xl">
+                        <p>Weststr. 70</p>
+                        <p>38126 Braunschweig</p>
+                    </div>
+                    <div class="flex flex-col gap-4 sm:flex-row lg:flex-col">
+                        <OpeningHours />
+                        <Contact />
+                    </div>
                 </div>
-                <div class="flex flex-col gap-4 sm:flex-row lg:flex-col">
-                    <OpeningHours />
-                    <Contact />
+                <div class="flex flex-col gap-4 mt-8 w-auto lg:w-6/12 lg:mt-0">
+                    <TeamImage />
+                    <ApplicationTeaser />
                 </div>
             </div>
-            <div class="flex flex-col gap-4 mt-8 w-auto lg:w-6/12 lg:mt-0">
-                <TeamImage />
-                <ApplicationTeaser />
-            </div>
+            <ServicesShort />
         </div>
     )
 }
---
+++ src/components/ServicesShort.tsx
@@ -0,0 +1,40 @@
+import { createResource, For } from 'solid-js';
+import { A } from 'solid-start';
+
+// type Student = { name: string; house: string; }
+type Service = { title: string; url: string; description: string; }
+
+function ServicesList(props: any) {
+
+    // const [students] = createResource(async () => {
+    //     const response = await fetch("https://hogwarts.deno.dev/students");
+    //     return await response.json() as Student[];
+    // });
+
+    const [services] = createResource(async () => {
+        const response = await fetch('/data/services.json');
+        return await response.json() as Service[];
+    }
+    );
+
+    return (
+        <div class={`${props.className}`}>
+            <h2 class='text-3xl font-special'>Unsere Leistungen</h2>
+            <ul>
+                {/* <For each={services()}> */}
+                {/* {(service) => ( */}
+                {services() && services()!.map((service) => (
+                    <A href={`/${service.url}`}>
+                        <li class='mt-2 pb-2 border-b-2'>
+                            <h3 class='text-xl'>{service.title}</h3>
+                            <p>{service.description}</p>
+                        </li>
+                    </A>
+                ))}
+                {/* </For> */}
+            </ul>
+        </div>
+    );
+}
+
+export default ServicesList;
