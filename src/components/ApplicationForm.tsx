b8b1eac4f5ac1ca59e1a00811992e22c4f81f778
Niklas <account@niklas.ai>
2023-12-30 15:01:42 +0100

    made ApplicationForm to a actual form, added data handling, submitting follows

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,11 +1,77 @@
+import { Component, createEffect, createSignal } from "solid-js";
 import { A } from "@solidjs/router";
 
 export default function ApplicationForm(props: any) {
+    const [name, setName] = createSignal("");
+    const [surname, setSurname] = createSignal("");
+    const [address, setAddress] = createSignal("");
+    const [email, setEmail] = createSignal("");
+    const [anschreiben, setAnschreiben] = createSignal("");
+
+    const handleSubmit = (event: Event): void => {
+        event.preventDefault();
+        const dataToSubmit = {
+            name: name(),
+            surname: surname(),
+            address: address(),
+            email: email(),
+            anschreiben: anschreiben(),
+        };
+
+        // should be sending data via POST request...
+        console.log(`submitting ${JSON.stringify(dataToSubmit)}`);
+    };
+
     return (
-        <div class={`${props.className} flex flex-col md:w-fit gap-2`}>
-            <input type="file" required>Dein Lebenslauf</input>
-            <input type="text" required>Vor- und Nachname</input>
-            <input type="text" required>Anschreiben</input>
+        <div class={`${props.className}`}>
+            <form onSubmit={handleSubmit}>
+                <div class="form-control">
+                    <label for="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name()}
+                        onChange={(e) => setName(e.currentTarget.value)}
+                    />
+                </div>
+                <div class="form-control">
+                    <label for="surname">Vorname:</label>
+                    <input
+                        type="text"
+                        id="surname"
+                        value={surname()}
+                        onChange={(e) => setSurname(e.currentTarget.value)}
+                    />
+                </div>
+                <div class="form-control">
+                    <label for="address">Adresse:</label>
+                    <input
+                        type="text"
+                        id="address"
+                        value={address()}
+                        onChange={(e) => setAddress(e.currentTarget.value)}
+                    />
+                </div>
+                <div class="form-control">
+                    <label for="email">E-Mail</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={email()}
+                        onChange={(e) => setEmail(e.currentTarget.value)}
+                    />
+                </div>
+                <div class="form-control">
+                    <label for="anschreiben">Anschreiben:</label>
+                    <input
+                        type="text"
+                        id="anschreiben"
+                        value={anschreiben()}
+                        onChange={(e) => setAnschreiben(e.currentTarget.value)}
+                    />
+                </div>
+                <input class="form-submit" type="submit" value="Bewerbung abschicken" />
+            </form>
         </div>
-    )
-}
+    );
+};
