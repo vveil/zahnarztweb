3be502f8cd73bf0c7cc611d9696747020728f2f7
Niklas <account@niklas.ai>
2023-12-31 12:21:11 +0100

    styling for application form, TODO: More fields, refine styling

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -24,53 +24,55 @@ export default function ApplicationForm(props: any) {
 
     return (
         <div class={`${props.className}`}>
-            <form onSubmit={handleSubmit}>
-                <div class="form-control">
-                    <label for="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        value={name()}
-                        onChange={(e) => setName(e.currentTarget.value)}
-                    />
+            <form onSubmit={handleSubmit} class="flex flex-col gap-4">
+                <div class="flex gap-4">
+                    <div class="form-control flex flex-col gap-2 flex-1">
+                        <label for="surname">Vorname</label>
+                        <input class="p-1"
+                            type="text"
+                            id="surname"
+                            value={surname()}
+                            onChange={(e) => setSurname(e.currentTarget.value)}
+                        />
+                    </div>
+                    <div class="form-control flex flex-col gap-2 flex-1">
+                        <label class="" for="name">Name</label>
+                        <input class="p-1"
+                            type="text"
+                            id="name"
+                            value={name()}
+                            onChange={(e) => setName(e.currentTarget.value)}
+                        />
+                    </div>
                 </div>
-                <div class="form-control">
-                    <label for="surname">Vorname:</label>
-                    <input
-                        type="text"
-                        id="surname"
-                        value={surname()}
-                        onChange={(e) => setSurname(e.currentTarget.value)}
-                    />
-                </div>
-                <div class="form-control">
-                    <label for="address">Adresse:</label>
-                    <input
+                <div class="form-control flex flex-col gap-2">
+                    <label for="address">Adresse</label>
+                    <input class="p-1"
                         type="text"
                         id="address"
                         value={address()}
                         onChange={(e) => setAddress(e.currentTarget.value)}
                     />
                 </div>
-                <div class="form-control">
+                <div class="form-control flex flex-col gap-2">
                     <label for="email">E-Mail</label>
-                    <input
+                    <input class="p-1"
                         type="text"
                         id="email"
                         value={email()}
                         onChange={(e) => setEmail(e.currentTarget.value)}
                     />
                 </div>
-                <div class="form-control">
-                    <label for="anschreiben">Anschreiben:</label>
-                    <input
-                        type="text"
+                <div class="form-control flex flex-col gap-2">
+                    <label for="anschreiben">Anschreiben</label>
+                    <textarea class="p-1"
+                        rows={7}
                         id="anschreiben"
                         value={anschreiben()}
                         onChange={(e) => setAnschreiben(e.currentTarget.value)}
                     />
                 </div>
-                <input class="form-submit" type="submit" value="Bewerbung abschicken" />
+                <input class="form-submit p-2 bg-highlight rounded text-light-teal" type="submit" value="Bewerbung abschicken" />
             </form>
         </div>
     );
