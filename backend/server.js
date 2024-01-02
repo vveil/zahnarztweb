1223815cf9e087b48c1934299b2060f5870d80bf
Niklas <account@niklas.ai>
2024-01-02 16:26:11 +0100

    moved port into const variable

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 3001;
 
 app.get("/", (req, res) => {
   res.send("Successful response.");
 });
 
-app.listen(3001, () => console.log("Example app is listening on port 3000."));
+app.listen(PORT, () =>
+  console.log(`Example app is listening on port ${PORT}.`)
+);
