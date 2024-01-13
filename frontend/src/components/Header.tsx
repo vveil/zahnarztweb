a6769af289e509264e06d564f24d48b743770570
Niklas <account@niklas.ai>
2024-01-13 23:25:18 +0100

    changed to sans serif in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header(props: any) {
                 <h1 class="text-start font-semibold text-xl md:text-2xl text-dark-blue">Zahnarztpraxis</h1>
                 <h1 class="text-start font-semibold text-xl md:text-2xl opacity-40">Dr. med. dent. Jürgen Werner</h1>
             </A>
-            <div class="hidden lg:flex gap-8 text-lg items-center font-special">
+            <div class="hidden lg:flex gap-8 text-lg items-center">
                 <A class={`${active("/openinghours")} `} href="/openinghours">Öffnungszeiten</A>
                 <A class={`${active("/directions")} `} href="/directions">Anfahrt</A>
                 <A class={`${active("/service")} `} href="/service">Behandlungsspektrum</A>
@@ -28,7 +28,7 @@ export default function Header(props: any) {
                 <A class={`${active("/team")} `} href="/team">Team</A>
                 <ApplicationButton text="Bewerben" className={`${active("/bewerben")} "hover:bg-highlight-light"`} />
             </div>
-            <div class="flex lg:hidden font-special">
+            <div class="flex lg:hidden">
                 Test
             </div>
         </div>
