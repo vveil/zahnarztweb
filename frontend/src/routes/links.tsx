ae5ed368091410cc49e8d7739cfe1187a25e5ba5
Niklas <account@niklas.ai>
2024-01-13 17:33:54 +0100

    added links route

---
+++ frontend/src/routes/links.tsx
@@ -0,0 +1,12 @@
+import EmployeePreview from "~/components/EmployeePreview";
+import SiteHeading from "~/components/SiteHeading";
+import BaseLayout from "~/layouts/BaseLayout";
+
+export default function Links() {
+  return (
+    <BaseLayout className="default-width">
+      <SiteHeading text='Links' className="mb-2" />
+      <p> In Arbeit </p>
+    </BaseLayout>
+  );
+}
