1445dd53959e72d3bc8ec06cbd42d855419cbb4f
Niklas <account@niklas.ai>
2024-01-22 16:39:57 +0100

    fixed anamanesebogen design

--- frontend/src/components/DefaultButton.tsx
+++ frontend/src/components/DefaultButton.tsx
@@ -4,7 +4,7 @@ export default function DefaultButton(props: any) {
     return (
         <A
             href={`${props.url}`}
-            class={`font-bold py-2 rounded-lg border-highlight text-highlight w-fit px-3 border-2 drop-shadow-solid bg-dark-teal`}
+            class={`default-button`}
         >
             {props.children}
         </A>
--- frontend/src/routes/dokumente.tsx
+++ frontend/src/routes/dokumente.tsx
@@ -8,14 +8,14 @@ export default function Dokumente(props: any) {
     return (
         <div>
             <Show when={anamneseFile && showAnamnese()}>
-                <button onClick={() => setShowAnamnese(false)}>←</button>
-                <iframe src={anamneseFile} class="w-full flex-1"></iframe>
+                <button class="mb-2 default-button" onClick={() => setShowAnamnese(false)}>← Zurück</button>
+                <iframe src={anamneseFile} class="w-full h-[62.5rem]"></iframe>
             </Show>
             <Show when={!showAnamnese()}>
                 <div class="flex flex-col sm:flex-row gap-8">
                     <BaseWidget title="Anamnesebogen" className="w-fit lg:min-w-[412px]">
                         <div class="flex gap-4">
-                            <button onClick={() => setShowAnamnese(true)} class="flex-1 font-bold py-2 rounded-lg border-highlight text-highlight px-3 border-2 bg-dark-teal">Bearbeiten<span class="ml-2">→</span></button>
+                            <button onClick={() => setShowAnamnese(true)} class="flex-1 default-button drop-shadow-none">Bearbeiten<span class="ml-2">→</span></button>
                             <A href="documents/anamesebogen.pdf" download="AnamnesebogenDrJürgenWerner.pdf" class="flex-1 primary-button text-center">Herunterladen</A>
                         </div>
                     </BaseWidget>
