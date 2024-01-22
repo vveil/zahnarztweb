97385ca9c6ad651ae24dba00e7266a8ba1bd3d89
Niklas <account@niklas.ai>
2024-01-22 17:34:06 +0100

    added links to /aktuelles

--- frontend/src/routes/aktuelles.tsx
+++ frontend/src/routes/aktuelles.tsx
@@ -1,11 +1,23 @@
 import { A } from "@solidjs/router";
+import BaseWidget from "~/components/BaseWidget";
+import HeroText from "~/components/HeroText";
 import BaseLayout from "~/layouts/BaseLayout";
 
 export default function Aktuelles() {
   return (
     <main class="">
       <BaseLayout className="">
-        <h1 class="text-4xl font-bold">Aktuelles</h1>
+        <div class="flex flex-col sm:flex-row sm:justify-between">
+          <HeroText text="Aktuelles" />
+          <BaseWidget title="Weiterführende Links" className="hidden sm:flex w-fit mx-5 sm:mx-0">
+            <div class="flex flex-col gap-1">
+              <A class="text-lg text-highlight hover:underline" href="https://www.kzvn.de/">Kassenzahnärztliche Vereinigung Niedersachsen</A>
+              <A class="text-lg text-highlight hover:underline" href="https://www.dgzs.de/">Deutsche Gesellschaft Zahnärztliche Schlafmedizin</A>
+              <A class="text-lg text-highlight hover:underline" href="https://share.rheinklang.de/player?link=64fb34a7cd3d7">"Zähne zeigen" eine Kampagne der Zahnärzteschaft</A>
+              {/* <A href="">Alles viel zu teuer - Flyer der KZVN</A> */}
+            </div>
+          </BaseWidget>
+        </div>
       </BaseLayout>
     </main>
   );
--- frontend/src/routes/dokumente.tsx
+++ frontend/src/routes/dokumente.tsx
@@ -1,6 +1,7 @@
 import { A } from "@solidjs/router"
 import { Show, createSignal } from "solid-js"
 import BaseWidget from "~/components/BaseWidget"
+import HeroText from "~/components/HeroText"
 
 export default function Dokumente(props: any) {
     const anamneseFile = 'documents/anamnesebogen.pdf'
@@ -12,14 +13,15 @@ export default function Dokumente(props: any) {
                 <iframe src={anamneseFile} class="w-full h-[62.5rem]"></iframe>
             </Show>
             <Show when={!showAnamnese()}>
-                <div class="flex flex-col sm:flex-row gap-8">
-                    <BaseWidget title="Anamnesebogen" className="w-fit lg:min-w-[412px]">
+                <HeroText text="Dokumente" className="mb-12" />
+                <div class="flex flex-col sm:flex-row gap-8 mx-5 sm:mx-0">
+                    <BaseWidget title="Anamnesebogen" className="sm:w-fit lg:min-w-[412px]">
                         <div class="flex gap-4">
-                            <button onClick={() => setShowAnamnese(true)} class="flex-1 default-button drop-shadow-none">Bearbeiten<span class="ml-2">→</span></button>
+                            <button onClick={() => setShowAnamnese(true)} class="flex-1 hidden sm:flex justify-center default-button drop-shadow-none">Bearbeiten<span class="ml-2">→</span></button>
                             <A href="documents/anamesebogen.pdf" download="AnamnesebogenDrJürgenWerner.pdf" class="flex-1 primary-button text-center">Herunterladen</A>
                         </div>
                     </BaseWidget>
-                    <BaseWidget title="Verhaltensmaßnahmen" className="w-fit lg:min-w-[412px]">
+                    <BaseWidget title="Verhaltensmaßnahmen" className="sm:w-fit lg:min-w-[412px]">
                         <div class="flex">
                             <A href="documents/verhaltensmaßnahmen.pdf" download="Verhaltenmaßnahmen.pdf" class="flex-1 primary-button text-center">Herunterladen</A>
                         </div>
