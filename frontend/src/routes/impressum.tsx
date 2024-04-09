56b3aa0ec95905033c5864172b5ac6a8652a7b3b
Niklas <account@niklas.ai>
2024-04-09 20:26:23 +0200

    updated impressum

--- frontend/src/routes/impressum.tsx
+++ frontend/src/routes/impressum.tsx
@@ -75,86 +75,59 @@ export default function Impressum() {
     <div class="mtl:mx-0 mx-5 flex flex-col gap-4">
       <h1 class="text-4xl font-bold">Impressum</h1>
 
-      <p>
-        J&uuml;rgen Werner
-        <br />
-        Zahnarztpraxis Dr. med. dent. J&uuml;rgen Werner
-        <br />
-        Weststra&szlig;e 70
-        <br />
-        381236 Braunschweig
-      </p>
+      <p>J&uuml;rgen Werner<br />
+        Zahnarztpraxis Dr. med. dent. J&uuml;rgen Werner<br />
+        Weststra&szlig;e 70<br />
+        38126 Braunschweig</p>
 
       <h2>Kontakt</h2>
-      <p>
-        Telefon: 0531 6802929
-        <br />
-        Telefax: 05316802930
-        <br />
-        E-Mail: dr.juergenwerner@arcor.de
-      </p>
+      <p>Telefon: 0531 / 6802929<br />
+        Telefax: 0531 / 6802930<br />
+        E-Mail: dr.juergenwerner@arcor.de</p>
+
+      <h2>Umsatzsteuer-ID</h2>
+      <p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+        13/147/00623</p>
 
       <h2>Aufsichtsbeh&ouml;rde</h2>
-      <p>
-        Kassenzahn&auml;rztliche Vereinigung Niedersachsen
-        <br />
-        Kassenzahn&auml;rztliche Vereinigung Niedersachsen
-        <br />
-        Zei&szlig;stra&szlig;e 11
-        <br />
-        30519 Hannover
-      </p>
-      <p>
-        <a
-          href="https://www.kzvn.de/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://www.kzvn.de/
-        </a>
-      </p>
+      <p>Kassenzahn&auml;rztliche Vereinigung Niedersachsen<br />
+        Zei&szlig;stra&szlig;e 11<br />
+        30519 Hannover</p>
+      <p><a href="https://kzvn.de" target="_blank" rel="noopener noreferrer">https://kzvn.de</a></p>
 
       <h2>Berufsbezeichnung und berufsrechtliche Regelungen</h2>
-      <p>
-        Berufsbezeichnung:
-        <br />
-        Zahnarzt
-      </p>
-      <p>
-        Zust&auml;ndige Kammer:
-        <br />
-        Zahn&auml;rztekammer Niedersachsen
-        <br />
-        Zei&szlig;stra&szlig;e 11a
-        <br />
-        30519 Hannover
-      </p>
-      <p>
-        Verliehen in:
-        <br />
-        Deutschland
-      </p>
+      <p>Berufsbezeichnung:<br />
+        Zahnarzt (Berufsbezeichnung verliehen in der Bundesrepublik Deutschland)</p>
+      <p>Zust&auml;ndige Kammer:<br />
+        Zahn&auml;rztekammer Niedersachsen<br />
+        Zei&szlig;stra&szlig;e 11a<br />
+        30519 Hannover</p>
+      <p>Verliehen in:<br />
+        Deutschland</p>
+      <p>Es gelten folgende berufsrechtliche Regelungen:</p>
+      <p>Heilberufe Kammergesetz<br />
+        einsehbar unter: <a href="https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/" target="_blank" rel="noopener noreferrer">https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/</a></p>
+      <p>Zahnheilkundegesetz<br />
+        einsehbar unter: <a href="https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/" target="_blank" rel="noopener noreferrer">https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/</a></p>
+      <p>Berufsordnung<br />
+        einsehbar unter: <a href="https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/" target="_blank" rel="noopener noreferrer">https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/</a></p>
+      <p>Geb&uuml;hrenordnung f&uuml;r Zahn&auml;rzte (GOZ)<br />
+        einsehbar unter: <a href="https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/" target="_blank" rel="noopener noreferrer">https://zkn.de/zahnaerzte-und-praxisteam/rechtliches-formulare/beruf-und-recht/</a></p>
       <h2>Angaben zur Berufs&shy;haftpflicht&shy;versicherung</h2>
+      <p><strong>Name und Sitz des Versicherers:</strong><br />
+        SIGNAL IDUNA Gruppe<br />
+        Hauptverwaltung Hamburg<br />
+        Neue Rabenstr. 15-19<br />
+        20354 Hamburg</p>
+      <p><strong>Geltungsraum der Versicherung:</strong><br />Deutschland</p>
+
       <h2>Redaktionell verantwortlich</h2>
-      <p>
-        Dr. med. dent. J&uuml;rgen Werner
-        <br />
-        Weststra&szlig;e 70
-        <br />
-        38126 Braunschweig
-      </p>
+      <p>Dr. med. dent. J&uuml;rgen Werner</p>
 
-      <h2>
-        Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
-      </h2>
-      <p>
-        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
-        vor einer Verbraucherschlichtungsstelle teilzunehmen.
-      </p>
+      <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+      <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
 
-      <p>
-        Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a>
-      </p>
+      <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
     </div>
   );
 }
