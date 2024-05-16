585fddfad0b1de6d3a760ba9f8444553927db09d
Niklas <account@niklas.ai>
2024-05-16 19:44:29 +0200

    changed language to de

--- frontend/src/entry-server.tsx
+++ frontend/src/entry-server.tsx
@@ -3,7 +3,7 @@ import { createHandler, StartServer } from "@solidjs/start/server";
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="de">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
