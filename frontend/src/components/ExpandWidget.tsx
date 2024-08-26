04dbf34e9d1029c306cb38632837fb07d9904b25
Niklas <account@niklas.ai>
2024-08-26 17:55:41 +0200

    added new routes, removed comment

--- frontend/src/components/ExpandWidget.tsx
+++ frontend/src/components/ExpandWidget.tsx
@@ -15,22 +15,3 @@ export default function ExpandWidget(props: any) {
     )
 }
 
-{/* 
-        <div class={`flex flex-col gap-5 rounded p-5 sm:p-7 pt-6 ${props.bgColor} ${props.className} `}>
-            <h2 class="text-2xl font-bold">{props.title}</h2>
-            <div class={`text-xl ${props.childrenDivClasses}`}>
-                {props.children}
-            </div>
-        </div>
-
-<div class={`flex flex-col gap-5 rounded p-5 sm:p-7 pt-6 ${props.bgColor} ${props.className} `}>
-            <div class={`flex justify-between align-middle rounded`}>
-                <h2 class="text-2xl font-bold">{props.title}</h2>
-                <div>Mehr erfahren\\/</div>
-            </div>
-            <div class={`text-xl ${props.childrenDivClasses}`}>
-                {props.children}
-            </div>
-        </div> 
-
-        */}
