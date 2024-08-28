83b305526b3bbb6a42978a9c9fc05be0df583c8f
Niklas <account@niklas.ai>
2024-08-28 19:01:24 +0200

    some stuff

--- frontend/src/components/ExpandWidget.tsx
+++ frontend/src/components/ExpandWidget.tsx
@@ -5,18 +5,23 @@ export default function ExpandWidget(props: any) {
         props.bgColor = "bg-dark-teal"
     }
     return (
-        <div class={`flex flex-col gap-5 rounded p-5 sm:p-7 pt-6 ${props.bgColor} ${props.className} hover:bg-darker-teal`}>
-
-            <A class={``} href={`/${props.title.toLowerCase()}`}>
-                <div class={`flex justify-between align-middle rounded`}>
+        <div class={`flex justify-between gap-5 rounded p-5 sm:p-7 pt-6 ${props.bgColor} ${props.className} hover:bg-darker-teal transition-colors duration-200 delay-300
+               hover:delay-500`}>
+            <div class="flex-col gap-4">
+                <div class={`flex align-middle rounded w-2/3`}>
                     <h2 class="text-2xl font-bold">{props.title}</h2>
-                    <div>→</div>
                 </div>
                 <div class={`text-xl ${props.childrenDivClasses}`}>
                     {props.children}
                 </div>
-            </A>
+            </div>
+            <div class="flex items-center">→</div>
         </div>
     )
-}
 
+
+}
+{/*
+            <A class={``} href={`/${props.title.toLowerCase()}`}>
+            </A>
+*/}
